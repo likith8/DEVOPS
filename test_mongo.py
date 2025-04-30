794fedd8e3bd500e3e9f40b56f6cdead48eb33ca
Likith M <likithm539@gmail.com>
2025-05-01 00:46:58 +0530

    test1

--- test_mongo.py
+++ test_mongo.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 import certifi
 
-uri = "mongodb+srv://likithm539:likith123@data.otirnyd.mongodb.net/ToDoData.List?retryWrites=true&w=majority&appName=Data"
+uri = "mongodb+srv://likithm539:likith123@data.otirnyd.mongodb.net/?retryWrites=true&w=majority&appName=Data"
 
 try:
     client = MongoClient(uri, tls=True, tlsCAFile=certifi.where())
